d9473b945c02391ed222fd35d409431be9112130
andreclerigo <andreclerigo@outlook.com>
2020-04-30 12:04:52 +0100

    Retirar website

--- labi2019-t3g02/aula11/map.js
+++
@@ -1,5 +0,0 @@
-var map = new L.Map("oMeuMapa", {center: [40.633258,-8.659097],zoom: 15});
-var osmUrl="http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
-var osmAttrib="Map data OpenStreetMap contributors";
-var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
-map.addLayer(osm);
